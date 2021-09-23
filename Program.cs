181278d81d61143988ff15991f8a04f7a5506ae6
ashcan1 <ashki444@gmail.com>
2021-09-23 19:49:15 +0100

    if

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+      
+            Console.Write("Enter your number :\t");
+            int num = int.Parse(Console.ReadLine());
+            if (num <= 10)
+            {
+                Console.WriteLine("valid number");
+             
+            }
+            else
+            { 
+                Console.WriteLine("eneter number between 1 - 10 ");
+            }
+            
+
+         
+            //  int num = int.Parse(Console.ReadLine());
+
+
+
+        }
+    }
+}
