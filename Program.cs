62eec0dae8e10cfe62dc21f7645bcaa91d32d5a5
ashcan1 <ashki444@gmail.com>
2021-09-23 20:12:56 +0100

    secod

--- Program.cs
+++ Program.cs
@@ -7,22 +7,39 @@ namespace Conditionals
     {
         public static void Main(string[] args)
         {
-      
+      /*1- Write a program and ask the user to enter a number. The number should be between 1 to 10. If the user enters a valid number, display "Valid" on the console. Otherwise, display "Invalid".
             Console.Write("Enter your number :\t");
             int num = int.Parse(Console.ReadLine());
             if (num <= 10)
             {
                 Console.WriteLine("valid number");
-             
             }
             else
             { 
                 Console.WriteLine("eneter number between 1 - 10 ");
             }
             
+            */
+      
+      ////////////////////////////////////////////////////////////////
+      //2- Write a program which takes two numbers from the console and displays the maximum of the two.
+     
+      int i;
+      Console.Write("Enter your first number :\t");
+      int num = int.Parse(Console.ReadLine());
+      
+      Console.Write("Enter your second number :\t");
+      int secNum = int.Parse(Console.ReadLine());
 
-         
-            //  int num = int.Parse(Console.ReadLine());
+      int sum = num + secNum;
+      Console.WriteLine(sum);
+//////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
 
 
 
